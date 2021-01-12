beb47d7f40d802f09e0f20fc2b12e466327ebae1
Pavlo Terletskyi <pavloterleckij2@gmail.com>
2021-01-12 14:13:33 +0200

    Updates

---
+++ precache-manifest.0112205a8ae42bcc63d8dcf21cb31d90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbd18d9ce54c27094ce002e388376d42",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0c0f89c89347c8fb1a5e",
+    "url": "./static/css/main.545e033f.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c0f89c89347c8fb1a5e",
+    "url": "./static/js/main.e1e09e9e.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
